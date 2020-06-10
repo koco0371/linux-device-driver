87da106faece68795b1b5ee05d5488fe424181b0
koco0371 <koco0371@gmail.com>
2020-06-10 13:51:15 +0900

    rm other files

--- day3/.koco.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/Desktop/github/linux-device-driver/day3/koco.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/pi/Desktop/github/linux-device-driver/day3/koco.ko /home/pi/Desktop/github/linux-device-driver/day3/koco.o /home/pi/Desktop/github/linux-device-driver/day3/koco.mod.o ;  true
--- day3/.tmp_versions/koco.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/Desktop/github/linux-device-driver/day3/koco.ko
-/home/pi/Desktop/github/linux-device-driver/day3/koco.o
-
